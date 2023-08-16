23a33157eab1173539127059461782dfd8f7bcb7
cnacha-mfu <81607567+cnacha-mfu@users.noreply.github.com>
2023-08-16 09:32:57 +0700

    first commit

---
+++ src/main/java/com/example/EchoClient.java
@@ -0,0 +1,32 @@
+package com.example;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class EchoClient {
+    public static void main(String[] args) throws IOException {
+        String serverAddress = "localhost";
+        int portNumber = 12345;
+
+        
+
+        try (Socket socket = new Socket(serverAddress, portNumber);
+             PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
+             BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+             BufferedReader stdIn = new BufferedReader(new InputStreamReader(System.in))) {
+            
+            String userInput;
+            while ((userInput = stdIn.readLine()) != null) {
+                out.println(userInput);
+                System.out.println("Server response: " + in.readLine());
+            }
+        } catch (IOException e) {
+            System.err.println("Error occurred: " + e.getMessage());
+        }
+    }
+} 
+
---
+++ src/main/java/com/example/EchoServer.java
@@ -0,0 +1,41 @@
+package com.example;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+/**
+ * Hello world!
+ *
+ */
+
+public class EchoServer {
+    public static void main(String[] args) throws IOException {
+    
+
+        int portNumber = 12345;
+
+        try (ServerSocket serverSocket = new ServerSocket(portNumber)) {
+            System.out.println("Server is listening on port " + portNumber);
+            
+            while (true) {
+                try (Socket clientSocket = serverSocket.accept();
+                     PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
+                     BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()))) {
+                        
+
+                    String inputLine;
+                    while ((inputLine = in.readLine()) != null) {
+                        System.out.println("Received from client: " + inputLine);
+                        out.println("Client says: " + inputLine);
+                    }
+                }
+            }
+        } catch (IOException e) {
+            System.err.println("Error occurred: " + e.getMessage());
+        }
+    }
+}
