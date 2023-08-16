4f8cd80388d437f9c67ba21cd07ac6e520907c42
cnacha-mfu <81607567+cnacha-mfu@users.noreply.github.com>
2023-08-16 09:38:21 +0700

    add webserver

---
+++ src/main/java/com/example/WebServer.java
@@ -0,0 +1,31 @@
+package com.example;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class WebServer {
+   
+    public static void main(String[] args) {
+        int webport = 80;
+         // web server that response Hello using ServerSocket
+          try (ServerSocket serverSocket = new ServerSocket(webport)) {
+            System.out.println("Server is listening on port " + webport);
+            
+            while (true) {
+                try (Socket clientSocket = serverSocket.accept();
+                     PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true)){
+
+                        out.println("<html><body><h1>Hello From Server</h1></body></html>");
+                    }
+                }
+        } catch (IOException e) {
+            System.err.println("Error occurred: " + e.getMessage());
+        }
+
+    }
+    
+}
